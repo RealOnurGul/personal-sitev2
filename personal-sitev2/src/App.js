d9e8311eee319e52789196f044e1a0b47162b1f2
Onur Gul <realonurgul@gmail.com>
2026-02-08 18:50:49 -0500

    remove footer

--- personal-sitev2/src/App.js
+++ personal-sitev2/src/App.js
@@ -5,7 +5,6 @@ import { Analytics } from '@vercel/analytics/react';
 import StaggeredMenu from './components/StaggeredMenu/StaggeredMenu';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-import Footer from './components/Footer';
 import GradualBlur from './components/GradualBlur/GradualBlur';
 import logoImage from './assets/onur.png';
 import './App.css';
@@ -46,8 +45,6 @@ function App() {
           <Route path="/projects" element={<Projects />} />
         </Routes>
       </div>
-      {/* The site-wide footer at the bottom */}
-      <Footer />
       <Analytics />
       {/* Constant blur at the bottom of the screen, full width, always visible */}
       <div
